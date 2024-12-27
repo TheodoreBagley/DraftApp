3d24d8e04e17211a858a13034493051305926e41
TheodoreBagley <teddybagley18@gmail.com>
2024-12-26 18:06:31 -0700

    config

--- draft_app/views.py
+++ draft_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def start_screen(request):
+    template = loader.get_template("draft_app/start_screen.html")
+
+    return HttpResponse(template.render())
